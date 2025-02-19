659d1ea20f6747bb7175f3a4b203391d9dc96f37
Broly 12 <brailynperezbatista@gmail.com>
2025-02-19 00:15:54 -0400

    Base Repository Completo

--- SGHR.Persistence/Base/BaseRepository.cs
+++ SGHR.Persistence/Base/BaseRepository.cs
@@ -1,40 +1,92 @@
 ﻿
+using Microsoft.EntityFrameworkCore;
 using SGHR.Domain.Base;
 using SGHR.Domain.Repository;
+using SGHR.Persistence.Context;
 using System.Linq.Expressions;
 
 namespace SGHR.Persistence.Base
 {
     public class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : class
     {
-        public Task<bool> ExistsAsync(Expression<Func<TEntity, bool>> filter)
+        private readonly SGHRContext _Context;
+        private DbSet<TEntity> Entity { get; set; }
+        private OperationResult Result = new OperationResult();
+
+        public BaseRepository(SGHRContext context)
         {
-            throw new NotImplementedException();
+            _Context = context;
+            Entity = _Context.Set<TEntity>();
         }
 
-        public Task<List<TEntity>> GetAllAsync()
+        public virtual async Task<TEntity> GetEntityByIdAsync(int id)
         {
-            throw new NotImplementedException();
+            return await Entity.FindAsync(id);
         }
 
-        public Task<OperationResult> GetAllAsync(Expression<Func<TEntity, bool>> filter)
+        public virtual async Task<OperationResult> UpdateEntityAsync(TEntity entity)
         {
-            throw new NotImplementedException();
+            try
+            {
+                Entity.Update(entity);
+                await _Context.SaveChangesAsync();
+                Result.Success = true;
+
+            }
+            catch (Exception ex)
+            {
+                Result.Success = false;
+                Result.Message = "Error Actualizando los datos";
+            }
+
+            return Result;
         }
 
-        public Task<TEntity> GetEntityByIdAsync(int id)
+        public virtual async Task<OperationResult> SaveEntityAsync(TEntity entity)
         {
-            throw new NotImplementedException();
+            try
+            {
+                Entity.Add(entity);
+                await _Context.SaveChangesAsync();
+                Result.Success = true;
+
+            }
+            catch (Exception ex)
+            {
+                Result.Success = false;
+                Result.Message = "Error ingresando  los datos";
+            }
+
+            return Result;
         }
 
-        public Task<OperationResult> SaveEntityAsync(TEntity entity)
+        public virtual async Task<List<TEntity>> GetAllAsync()
         {
-            throw new NotImplementedException();
+            return await Entity.ToListAsync();
+
         }
 
-        public Task<OperationResult> UpdateEntityAsync(TEntity entity)
+        public virtual async Task<OperationResult> GetAllAsync(Expression<Func<TEntity, bool>> filter)
         {
-            throw new NotImplementedException();
+            try
+            {
+                await Entity.Where(filter).ToListAsync();
+                Result.Success = true;
+
+            }
+            catch (Exception ex)
+            {
+                Result.Success = false;
+                Result.Message = "Error Obteniendo los datos";
+            }
+
+            return Result;
+        }
+
+        public virtual async Task<bool> ExistsAsync(Expression<Func<TEntity, bool>> filter)
+        {
+            return await Entity.AnyAsync(filter);
         }
     }
 }
+
