b6462c1ff88ef94f38a46ba735d6a693dd1468f8
Broly 12 <brailynperezbatista@gmail.com>
2025-02-18 23:25:10 -0400

    Agregado OperationResult

---
+++ SGHR.Domain/Base/OperationResult.cs
@@ -0,0 +1,14 @@
+﻿
+namespace SGHR.Domain.Base
+{
+    public class OperationResult
+    {
+        public OperationResult()
+        {
+            this.Success = true;
+        }
+        public string Message { get; set; }
+        public bool Success { get; set; }
+        public dynamic Data { get; set; }
+    }
+}
---
+++ SGHR.Domain/Repository/IBaseRepository.cs
@@ -0,0 +1,17 @@
+﻿
+using SGHR.Domain.Base;
+using System.Linq.Expressions;
+
+namespace SGHR.Domain.Repository
+{
+    public interface IBaseRepository<TEntity> where TEntity : class
+    {
+        Task<TEntity> GetEntityByIdAsync(int id);
+        Task<OperationResult> UpdateEntityAsync(TEntity entity);
+        Task<OperationResult> SaveEntityAsync(TEntity entity);
+        Task<List<TEntity>> GetAllAsync();
+        Task<OperationResult> GetAllAsync(Expression<Func<TEntity, bool>> filter);
+        Task<bool> ExistsAsync(Expression<Func<TEntity, bool>> filter);
+
+    }
+}
