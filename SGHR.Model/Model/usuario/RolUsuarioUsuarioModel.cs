87865dde1ceea1ddca2751d3de4722cdbf651592
Atlast19 <ahleandro18@gmail.com>
2025-02-19 20:22:06 -0400

    ClientesReservas y RolUsuarioUsuario

---
+++ SGHR.Model/Model/usuario/RolUsuarioUsuarioModel.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace SGHR.Model.Model.usuario
+{
+    public class RolUsuarioUsuarioModel
+    {
+        public int IdUsuario { get; set; }
+        public int IdRolUsuario { get; set; }
+        public string NombreCompleto { get; set; }
+        public string Clave { get; set; }
+        public string Descripcion { get; set; }
+        public bool? Estado { get; set; }
+        public DateTime FechaCreacion { get; set; }
+    }
+}
--- SGHR.Persistence/Repositories/usuario/ClienteRepository.cs
+++ SGHR.Persistence/Repositories/usuario/ClienteRepository.cs
@@ -1,11 +1,14 @@
 ﻿
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using SGHR.Domain.Base;
 using SGHR.Domain.Entities.usuario;
+using SGHR.Model.Model.usuario;
 using SGHR.Persistence.Base;
 using SGHR.Persistence.Context;
 using SGHR.Persistence.Interfaces.usuario;
-using SGHR.Persistence.Repositories.habitacion;
+
 
 namespace SGHR.Persistence.Repositories.usuario
 {
@@ -20,6 +23,46 @@ namespace SGHR.Persistence.Repositories.usuario
             _context = context;
             _logger = logger;
             _configuration = configuration;
+        }
+
+        public async Task<OperationResult> GetCienteByReservas(Cliente cliente)
+        {
+            OperationResult result = new OperationResult();
+            try
+            {
+                var Query = await (from c in _context.Clientes
+                                   join r in _context.Reservas on c.IdCliente equals r.IdCliente
+                                   where r.IdCliente == cliente.IdCliente
+                                   select new ClienteReservasModel()
+                                   {
+                                       IdCliente = c.IdCliente,
+                                       IdReserva = r.IdReserva,
+                                       IdHabitacion = r.IdHabitacion,
+                                       TipoDocumento = c.TipoDocumento,
+                                       Documento = c.Documento,
+                                       NombreCompleto = c.NombreCompleto,
+                                       Correo = c.Correo,
+                                       FechaEntrada = r.FechaEntrada,
+                                       PrecioInicial = r.PrecioInicial,
+                                       Adelanto = r.Adelanto,
+                                       PrecioRestante = r.PrecioRestante,
+                                       CostoPenalidad = r.CostoPenalidad,
+                                       Observacion = r.Observacion,
+                                       TotalPagado = r.TotalPagado,
+                                       Estado = r.Estado,
+
+
+                                   }).ToListAsync();
+                result.Data = Query;
+            }
+            catch (Exception ex) 
+            {
+                result.Message = this._configuration["ErrorClienteRepository:GetCienteByReservas"];
+                result.Success = false;
+                this._logger.LogError(result.Message, ex.ToString());
+            }
+
+            return result;
         }
     }
 }
