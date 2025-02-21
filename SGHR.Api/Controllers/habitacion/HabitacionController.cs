8e954ac6da4cfaf5d4079d41e1593b3590787137
Broly 12 <brailynperezbatista@gmail.com>
2025-02-21 12:46:58 -0400

    Configuracion Base Creada

---
+++ SGHR.Api/Controllers/habitacion/HabitacionController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SGHR.Api.Controllers.habitacion
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HabitacionController : ControllerBase
+    {
+        
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+       
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
