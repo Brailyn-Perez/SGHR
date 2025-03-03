1f387dbbb66828e033872f845a64cb62ae9fd448
Francisco Castro <borrome941@gmail.com>
2025-03-02 21:08:08 -0400

    BL of service and reserva

---
+++ SGHR.Application/DTos/reserva/Reserva/UpdateReservaDTO.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace SGHR.Application.DTos.reserva.Reserva
+{
+    public class UpdateReservaDTO : ReservaBaseDTO
+    {
+        public int IdReserva { get; set; }
+        public DateTime FechaSalidaConfirmacion { get; set; }
+        public decimal PrecioRestante { get; set; }
+        public decimal TotalPagado { get; set; }
+        public decimal CostoPenalidad { get; set; }
+        public bool? Estado { get; set; }
+    }
+}
---
+++ SGHR.Application/Service/servicio/ServicioService.cs
@@ -0,0 +1,51 @@
+﻿
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using SGHR.Application.DTos.servicio.Servicio;
+using SGHR.Application.Interfaces.sevicio;
+using SGHR.Application.Service.habitacion;
+using SGHR.Domain.Base;
+using SGHR.Persistence.Interfaces.habitacion;
+
+namespace SGHR.Application.Service.servicio
+{
+    public class ServicioService : IServiciosService
+    {
+        private readonly ICategoriaRepository _Repository;
+        private readonly IConfiguration _configuration;
+        private readonly ILogger<CategoriaService> _logger;
+
+        public ServicioService(ICategoriaRepository repository, IConfiguration configuration, ILogger<CategoriaService> logger)
+        {
+            _Repository = repository;
+            _configuration = configuration;
+            _logger = logger;
+        }
+
+        public Task<OperationResult> GeAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OperationResult> GeById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OperationResult> Remove(RemoveServicioDTO dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OperationResult> Save(SaveServicioDTO dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OperationResult> Update(UpdateServicioDTO dto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
