50b1d13a6f9e77422e6f6f3dd9f8696141d84939
Broly 12 <brailynperezbatista@gmail.com>
2025-03-11 02:19:41 -0400

    Preparando el terreno para terminar mañana

--- SGHR.Persistence.Test/habitacion/UnitTestCategoriaRepository.cs
+++ SGHR.Persistence.Test/habitacion/UnitTestCategoriaRepository.cs
@@ -23,6 +23,8 @@ namespace SGHR.Persistence.Test.habitacion
             _categoriaRepository = new CategoriaRepository(_mockContext.Object, _mockLogger.Object, _mockConfiguration.Object);
         }
 
+        #region "SaveEntity"
+
         [Fact]
         public async Task SaveEntityAsync_ShouldReturnSuccess_WhenEntityIsSaved()
         {
@@ -35,5 +37,75 @@ namespace SGHR.Persistence.Test.habitacion
             // Assert
             Assert.True(result.Success);
         }
+
+        [Fact]
+        public async Task SaveEntityAsync_ShouldReturnSuccess_WhenEntityIsNull()
+        {
+
+        }
+
+        [Fact]
+        public async Task SaveEntityAsync_ShouldReturnSuccess_WhenEntityExist()
+        {
+
+        }
+
+        [Fact]
+        public async Task SaveEntityAsync_ShouldReturnSuccess_WhenCategoriaParamethersIsNull()
+        {
+
+        }
+        #endregion
+
+        #region "UpdateEntityAsync"
+        [Fact]
+        public async Task UpdateEntityAsync_ShouldReturnSuccess_WhenEntityIsUpdate()
+        {
+        }
+
+        [Fact]
+        public async Task UpdateEntityAsync_ShouldReturnSuccess_WhenEntityIsNull()
+        {
+
+        }
+
+        [Fact]
+        public async Task UpdateEntityAsync_ShouldReturnSuccess_WhenEntityDontExist()
+        {
+
+        }
+
+        [Fact]
+        public async Task UpdateEntityAsync_ShouldReturnSuccess_WhenCategoriaParamethersIsNull()
+        {
+
+        }
+        #endregion
+
+        #region "GetAllAsync"
+        #endregion
+
+        #region "GetEntityByIdAsync"
+        [Fact]
+        public async Task GetEntityByIdAsync_ShouldReturnSuccess_WhenIdIsNull()
+        {
+
+        }
+
+        [Fact]
+        public async Task GetEntityByIdAsync_ShouldReturnSuccess_WhenIdIsNegative()
+        {
+
+        }
+
+        [Fact]
+        public async Task GetEntityByIdAsync_ShouldReturnSuccess_WhenIdExceedsTheRange()
+        {
+
+        }
+        #endregion
+
+        #region "GetHabitacionByCategoriaId"
+        #endregion
     }
 }
